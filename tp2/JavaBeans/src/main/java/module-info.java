dff2489e2078981f8194b44d429884a5490af875
Raquel Sousa <raquelmcoelhodesousa@gmail.com>
2025-03-06 10:42:39 +0100

    fix: fixing error creating project again

---
+++ tp2/JavaBeans/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module org.example.javabeans {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens org.example.javabeans to javafx.fxml;
+    exports org.example.javabeans;
+}
