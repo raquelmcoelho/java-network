c7cb6154fec6640edb731339469adeb4afc29e2e
Raquel Sousa <raquelmcoelhodesousa@gmail.com>
2025-04-11 05:49:29 +0200

    chore: changing create statement

--- tp3/technojava_tp3/src/main/resources/META-INF/sql/create.sql
+++ tp3/technojava_tp3/src/main/resources/META-INF/sql/create.sql
@@ -1,24 +1,24 @@
 CREATE TABLE TODO (
     ID_TODO BIGINT not null,
-    DESCRIPTION VARCHAR(200) not null,
+    DESCRIPTION VARCHAR(255) not null,
     primary key (ID_TODO)
 );
 
 CREATE TABLE adherent (
   numero_adherent INT PRIMARY KEY,
-  nom VARCHAR(50),
-  prenom VARCHAR(50),
+  nom VARCHAR(255),
+  prenom VARCHAR(255),
   adresse VARCHAR(255),
-  telephone VARCHAR(20),
-  email VARCHAR(100),
+  telephone VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
   password VARCHAR(255)
 );
 
 CREATE TABLE tournoi (
  code_tournoi INT PRIMARY KEY,
- nom VARCHAR(100),
+ nom VARCHAR(255),
  date DATE,
- lieu VARCHAR(100)
+ lieu VARCHAR(255)
 );
 
 CREATE TABLE Inscription (
@@ -26,6 +26,6 @@ CREATE TABLE Inscription (
  code_tournoi INT,
  date_inscription DATE,
  PRIMARY KEY (numero_adherent, code_tournoi),
- FOREIGN KEY (numero_adherent) REFERENCES adherent(numero_adherent),
- FOREIGN KEY (code_tournoi) REFERENCES tournoi(code_tournoi)
+ FOREIGN KEY (code_tournoi) REFERENCES tournoi(code_tournoi) ON DELETE CASCADE,
+ FOREIGN KEY (numero_adherent) REFERENCES adherent(numero_adherent) ON DELETE CASCADE
 );
